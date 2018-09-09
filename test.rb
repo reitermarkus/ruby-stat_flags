13a95073755c19b20c1cf72ba2032cdffa7cd5aa
Markus Reiter <me@reitermark.us>
2018-09-09 15:09:31 +0200

    Add success output.

--- test.rb
+++ test.rb
@@ -17,8 +17,10 @@ Dir.chdir File.expand_path("../ext", __FILE__) do |dir|
   Dir.mktmpdir do |dir|
     system "chflags", "hidden", dir
 
-    if (flags = File.stat(dir).flags) != 0o100000
-      puts "0o#{flags.to_s(8)} != 0o100000"
+    if (flags = File.stat(dir).flags) == 0o100000
+      puts "0o#{flags.to_s(8)} == 0o100000"
+    else
+      $stderr.puts "0o#{flags.to_s(8)} != 0o100000"
       exit 1
     end
   end
